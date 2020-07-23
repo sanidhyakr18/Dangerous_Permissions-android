4841fbef19d40c8ecd7594e16a8622879e514152
sanidhyakr18 <sanidhyakr18@gmail.com>
2020-07-23 12:48:09 +0530

    Initial commit

---
+++ app/src/main/java/com/sandystudios/dangerouspermissions/MainActivity.kt
@@ -0,0 +1,43 @@
+package com.sandystudios.dangerouspermissions
+
+import android.Manifest
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.core.app.ActivityCompat
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        btnDial.setOnClickListener {
+            val telNo = etPhNo.text.toString()
+            val uri = Uri.parse("tel:$telNo")
+            val i = Intent(Intent.ACTION_CALL, uri)
+            if (ActivityCompat.checkSelfPermission(
+                    this,
+                    Manifest.permission.CALL_PHONE
+                ) != PackageManager.PERMISSION_GRANTED
+            ) {
+                ActivityCompat.requestPermissions(
+                    this,
+                    arrayOf(Manifest.permission.CALL_PHONE), 121
+                )
+                // TODO: Consider calling
+                //    ActivityCompat#requestPermissions
+                // here to request the missing permissions, and then overriding
+                //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                //                                          int[] grantResults)
+                // to handle the case where the user grants the permission. See the documentation
+                // for ActivityCompat#requestPermissions for more details.
+//                return
+            }
+            startActivity(i)
+        }
+
+    }
+}
